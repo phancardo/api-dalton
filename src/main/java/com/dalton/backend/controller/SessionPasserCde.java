d796810c02eae31ccfc4c9229c0cd9de002cd9dc
miraants <iantsamirantsoa@gmail.com>
2024-06-12 10:01:26 +0300

    Authentification utilisateur

--- src/main/java/com/dalton/backend/controller/SessionPasserCde.java
+++ src/main/java/com/dalton/backend/controller/SessionPasserCde.java
@@ -1,11 +1,13 @@
 package com.dalton.backend.controller;
 
 import com.dalton.backend.metierRelationnel.Produit;
+import com.dalton.backend.metierRelationnel.Utilisateur;
 import com.dalton.backend.metierServices.ProduitManager;
+import com.dalton.backend.metierServices.UtilisateurManager;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,14 +16,25 @@ import java.util.List;
 public class SessionPasserCde {
 
     ProduitManager produitManager;
+    private final UtilisateurManager utilisateurManager;
     @Autowired
-    public SessionPasserCde(ProduitManager produitManager) {
+    public SessionPasserCde(ProduitManager produitManager, UtilisateurManager utilisateurManager) {
         this.produitManager = produitManager;
+        this.utilisateurManager = utilisateurManager;
     }
 
     @GetMapping
     public List<Produit> traiterAccesApplication(){
         List<Produit> produits = produitManager.rechercherProduitEstDuJour(true);
         return produits;
+    }
+    @PostMapping("/login")
+    public ResponseEntity<Utilisateur> login(@RequestBody Utilisateur utilisateur) {
+        Utilisateur utilisateurTrouve = utilisateurManager.findByEmailAndMotDePasse(utilisateur.getEmail(), utilisateur.getMotDePasse());
+        if (utilisateurTrouve != null) {
+            return ResponseEntity.ok(utilisateurTrouve);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+        }
     }
 }
